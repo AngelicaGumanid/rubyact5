a9cf6bde9f5503b1953909cea9e0531c04d8ef1a
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-10-04 16:02:39 +0800

    Migration to add a new column image.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_04_074717) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_04_075747) do
   create_table "products", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_04_074717) do
     t.decimal "discount", precision: 10
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
